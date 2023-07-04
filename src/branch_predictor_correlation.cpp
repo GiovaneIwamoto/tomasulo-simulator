356fe55b36554f0ea4fc8f7000af7b06d2e6a2a8
Rafael <reoshiro13@gmail.com>
2023-07-04 10:42:21 -0300

    fix: parametres from reorder_buffer

--- src/branch_predictor_correlation.cpp
+++ src/branch_predictor_correlation.cpp
@@ -1,37 +1,39 @@
+#include <cmath>
 #include "branch_predictor_correlation.hpp"
 
 branch_predictor_correlation::branch_predictor_correlation(unsigned int m, unsigned int n) : m(m), n(n)
 {
-    max = (1 << n) - 1;
-    bms = (1 << (n - 1));
-    tabel.resize(1 << m, 0);
     c_predictions = 0;
     c_hits = 0;
+
+    max = (1 << n) - 1;
+
+    table.resize((1 << m), 0);
 }
 
 bool branch_predictor_correlation::predict_mn(unsigned int pc)
 {
     c_predictions++;
-    return (tabel[pc % (1 << m)] & bms);
+
+    return table[pc % (1 << m)] & (1 << (n - 1));
 }
 
 void branch_predictor_correlation::mn_update_state(unsigned int pc, bool taken, bool hit)
 {
-    int index = pc % (1 << m);
-    int temp = tabel[index];
-
     c_hits += hit;
+
+    int aux = table[pc % (1 << m)];
 
     if (taken)
     {
-        temp = ++temp > max ? max : temp;
+        aux += (aux < max);
     }
     else
     {
-        temp = --temp < 0 ? 0 : temp;
+        aux -= (aux > 0);
     }
 
-    tabel[index] = temp;
+    table[pc % (1 << m)] = aux;
 }
 
 float branch_predictor_correlation::get_predictor_mn_hit_rate()
--- src/branch_predictor_correlation.hpp
+++ src/branch_predictor_correlation.hpp
@@ -1,16 +1,18 @@
+#include <iostream>
 #include <vector>
 
 class branch_predictor_correlation
 {
+private:
+    std::vector<int> table;
+    unsigned int m, n;
+    unsigned int c_predictions;
+    int max;
+    int c_hits;
+
 public:
     branch_predictor_correlation(unsigned int m, unsigned int n);
     bool predict_mn(unsigned int pc);
     void mn_update_state(unsigned int pc, bool taken, bool hit);
     float get_predictor_mn_hit_rate();
-
-private:
-    unsigned int m, n;
-    int max, bms;
-    std::vector<int> tabel;
-    int c_predictions, c_hits;
 };
